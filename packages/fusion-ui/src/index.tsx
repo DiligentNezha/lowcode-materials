7aa66adf5cd24486d2eab2d9b4503e0a1e842ccd
景凯辉 <jingkaihui@jingkaihuideMacBook-Pro.local>
2022-07-17 21:23:34 +0800

    增加ant design chart

---
+++ packages/fusion-ui/src/index.tsx
@@ -0,0 +1,36 @@
+import { version } from '../package';
+import { ProCard } from '@alifd/pro-layout';
+
+// export { default as ProCard } from './components/pro-card';
+// export type { CardProps, CardSectionProps } from './components/pro-card';
+
+export { default as PieChart } from './components/pie-chart';
+export { default as ColumnChart } from './components/column-chart';
+export { default as BarChart } from './components/bar-chart';
+export { default as LineChart } from './components/line-chart';
+export { default as AreaChart } from './components/area-chart';
+export { default as DonutChart } from './components/donut-chart';
+export { default as LiquidChart } from './components/liquid-chart';
+export { default as ProgressChart } from './components/progress-chart';
+
+export { default as AntColumnChart } from './components/ant-column-chart';
+
+export * from './components/page-header';
+export * from './components/pro-form';
+export * from './components/pro-table';
+export * from './components/expand-table';
+export * from './components/filter';
+export * from './components/anchor';
+export * from './components/pro-dialog';
+export * from './components/tab-container';
+export * from './components/pro-drawer';
+export * from './components/button';
+export * from './components/button-group';
+export { default as StoryPlaceholder } from './components/story-placeholder';
+
+export * from './variables';
+
+const bizCssPrefix = 'fusion-ui';
+const displayName = 'FusionUI';
+
+export { bizCssPrefix, displayName, version, ProCard };
